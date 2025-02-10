5f96f1c036993b7be41a0165b12002d2b1aa52d5
kuylek-212 <keprim2029@gmail.com>
2025-02-10 22:06:12 +0700

    Create ad.js

---
+++ ad.js
@@ -0,0 +1,14 @@
+function redirectToAdmin(event) {
+    event.preventDefault(); // ป้องกันการโหลดหน้าใหม่
+
+    let password = document.getElementById("password").value;
+
+    // สามารถกำหนดรหัสผ่านที่ถูกต้องได้ที่นี่
+    let correctPassword = "1234"; 
+
+    if (password === correctPassword) {
+        window.location.href = "AdminStatus.html"; // เปลี่ยนหน้าไปที่ AdminStatus.html
+    } else {
+        alert("รหัสผ่านผิด!"); // แจ้งเตือนถ้ารหัสผิด
+    }
+}
